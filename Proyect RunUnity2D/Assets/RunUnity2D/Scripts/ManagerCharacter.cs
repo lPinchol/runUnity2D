65b2c1ff0392f9a8f1f32a8f65bdf09e9a2c34f7
lPinchol <pinchodev91@gmail.com>
2015-03-28 23:51:13 +0100

    Update Character :repeat:
    Update Character :repeat:

--- Proyect RunUnity2D/Assets/RunUnity2D/Scripts/ManagerCharacter.cs
+++ Proyect RunUnity2D/Assets/RunUnity2D/Scripts/ManagerCharacter.cs
@@ -4,5 +4,15 @@ using System.Collections;
 
 public class ManagerCharacter : MonoBehaviour
 {
+    //Var public
+    public float forceJump = 100.0f;                // Force to jump character
 
+    private void Update()
+    {
+        // if get button mouse 0 or touch (SmartPhone), character jump
+        if (Input.GetMouseButtonDown(0))
+        { 
+        
+        }
+    }
 }
