d718dbb19e3d9542ee11cef90f25278717b0e212
lPinchol <pinchodev91@gmail.com>
2015-03-29 00:13:34 +0100

    Update MC.cs :up:
    Update MC.cs :up:

--- Proyect RunUnity2D/Assets/RunUnity2D/Scripts/ManagerCharacter.cs
+++ Proyect RunUnity2D/Assets/RunUnity2D/Scripts/ManagerCharacter.cs
@@ -6,13 +6,21 @@ public class ManagerCharacter : MonoBehaviour
 {
     //Var public
     public float forceJump = 100.0f;                // Force to jump character
+    public GameObject characterRigi;
+
+    private void Start()
+    {
+        // Assig character - var
+        characterRigi = this.gameObject;
+    }
 
     private void Update()
     {
-        // if get button mouse 0 or touch (SmartPhone), character jump
+        // if get button mouse 0 or touch (SmartPhone), character run
         if (Input.GetMouseButtonDown(0))
-        { 
-        
+        {
+            // Add force in X for run
+            characterRigi.GetComponent<Rigidbody2D>().AddForce(new Vector2(0, forceJump));
         }
     }
 }
