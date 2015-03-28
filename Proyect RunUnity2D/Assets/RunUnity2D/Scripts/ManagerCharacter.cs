049951ce4ac48925c3312d2dba6095682572d392
lPinchol <pinchodev91@gmail.com>
2015-03-28 23:46:20 +0100

    Create Machine State & Physics :m:
    Create Machine State & Physics :m:

---
+++ Proyect RunUnity2D/Assets/RunUnity2D/Scripts/ManagerCharacter.cs
@@ -0,0 +1,50 @@
+﻿#region Dev Date
+//*******************************************************
+//
+//	Script:					ManagerCharacter.cs ("Tipo")    
+//	Developer:     			MiiviCO  
+//	Autor:					Antonio Mateo Tomas (Pincho) - (00/Mes/0000)
+//
+//	Descripcion:        
+//		----
+//
+//	Historial(00/Mes/0000): 
+//		----
+// 
+//******************************************************
+#endregion
+
+#region Librerias
+using UnityEngine;
+using System.Collections;
+#endregion
+
+/// <summary>
+/// <para></para>
+/// </summary>
+public class ManagerCharacterManagerCharacter : MonoBehaviour
+{
+    #region Variables Publicas
+    #endregion
+
+    #region Variables Privadas
+    #endregion
+
+    #region Metodos Comunes
+    /// <summary>
+    /// Carga cuando Inicializa la Escena
+    /// </summary>
+    private void Start()// Carga cuando Inicializa la Escena
+    {
+
+    }
+
+    /// <summary>
+    /// Se ejecuta cada frame
+    /// </summary>
+    private void Update()// Se ejecuta cada frame
+    {
+
+    }
+    #endregion
+}
