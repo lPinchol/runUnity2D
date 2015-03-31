a4aa1e4e256614b8253b01eda8bc8a4f4f49ff89
lPinchol <pinchodev91@gmail.com>
2015-03-31 07:40:11 +0200

    Generator terrain alfa :top:
    Generator terrain alfa :top:

--- Proyect RunUnity2D/Assets/RunUnity2D/Scripts/Generator.cs
+++ Proyect RunUnity2D/Assets/RunUnity2D/Scripts/Generator.cs
@@ -7,4 +7,15 @@ public class Generator : MonoBehaviour
     public GameObject[] obj;
     public float minTime = 1.0f;
     public float maxTime = 2.0f;
+
+    private void Start()
+    {
+        Generate();
+    }
+
+    private void Generate()
+    {
+        Instantiate(obj[Random.Range(0, obj.Length)],transform.position,Quaternion.identity);
+        Invoke("Generate",Random.Range(minTime,maxTime));
+    }
 }
